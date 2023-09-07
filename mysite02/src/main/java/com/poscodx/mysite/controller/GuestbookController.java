d2fdeb378bd63de4051571c0932ebe6b5419b43c
[talkingOrange] <sonrisa-bonita@naver.com>
2023-09-07 17:09:41 +0900

    [ feat ] mysite02 - guestbook (select, delete)

---
+++ mysite02/src/main/java/com/poscodx/mysite/controller/GuestbookController.java
@@ -0,0 +1,64 @@
+package com.poscodx.mysite.controller;
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.poscodx.mysite.dao.GuestbookDao;
+import com.poscodx.mysite.vo.GuestbookVo;
+
+public class GuestbookController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		request.setCharacterEncoding("utf-8");
+		String action = request.getParameter("a");
+
+		if ("insert".equals(action)) {
+
+			String name = request.getParameter("name");
+			String password = request.getParameter("pass");
+			String contents = request.getParameter("content");
+
+			GuestbookVo vo = new GuestbookVo();
+			vo.setName(name);
+			vo.setPassword(password);
+			vo.setContents(contents);
+
+			new GuestbookDao().insert(vo);
+			System.out.println(vo);
+			response.sendRedirect("/mysite02/guestbook");
+
+		} else if ("delete".equals(action)) {
+			String password = request.getParameter("password");
+
+			GuestbookVo vo = new GuestbookVo();
+			vo.setPassword(password);
+
+			new GuestbookDao().deleteByPassword(password);
+			response.sendRedirect("/mysite02/guestbook");
+		} else if ("deleteform".equals(action)) {
+			RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/guestbook/deleteform.jsp");
+			rd.forward(request, response);
+
+		} else {
+			List<GuestbookVo> list = new GuestbookDao().findAll();
+			request.setAttribute("list", list);
+			request.getRequestDispatcher("/WEB-INF/views/guestbook/index.jsp").forward(request, response);
+
+		}
+
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
--- mysite02/src/main/java/com/poscodx/mysite/controller/UserController.java
+++ mysite02/src/main/java/com/poscodx/mysite/controller/UserController.java
@@ -6,13 +6,19 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-public class MainController extends HttpServlet {
+public class UserController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
+       
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		request
-			.getRequestDispatcher("/WEB-INF/views/main/index.jsp")
-			.forward(request, response);
+		request.setCharacterEncoding("utf-8");
+		
+		String action = request.getParameter("a");
+		
+		if("joinform".equals(action)) {
+			request.getRequestDispatcher("/WEB-INF/views/user/joinform.jsp").forward(request, response);
+		} 
+		
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
@@ -20,4 +26,3 @@ public class MainController extends HttpServlet {
 	}
 
 }
-
