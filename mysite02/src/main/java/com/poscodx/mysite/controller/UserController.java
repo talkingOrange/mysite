8dd43cf2c296696f4a5d32b4c04e007d21e28b8c
[talkingOrange] <sonrisa-bonita@naver.com>
2023-09-08 11:11:12 +0900

    [ feat ] mysite02 - actin interface (split component)

--- mysite02/src/main/java/com/poscodx/mysite/controller/UserController.java
+++ mysite02/src/main/java/com/poscodx/mysite/controller/UserController.java
@@ -10,39 +10,41 @@ import org.apache.catalina.ant.jmx.JMXAccessorQueryTask;
 
 import com.poscodx.mysite.dao.UserDao;
 import com.poscodx.mysite.vo.UserVo;
+import com.poscodx.web.mvc.Action;
+import com.poscodx.web.mvc.user.JoinAction;
+import com.poscodx.web.mvc.user.JoinFormAction;
+import com.poscodx.web.mvc.user.JoinSuccessAction;
 
 public class UserController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
 
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		request.setCharacterEncoding("utf-8");
-		
-		String action = request.getParameter("a");
-		
-		if("joinform".equals(action)) {
-			request.getRequestDispatcher("/WEB-INF/views/user/joinform.jsp").forward(request, response);
-		} else if("join".equals(action)) {
-			String name = request.getParameter("name");
-			String email = request.getParameter("email");
-			String password = request.getParameter("password");
-			String gender = request.getParameter("gender");
-			
-			//데이터 전달의 확인
-			//System.out.println(name + " : " + email + " : " + password  + " : " + gender);
-		
-			UserVo userVo = new UserVo();
-			userVo.setName(name);
-			userVo.setEmail(email);
-			userVo.setPassword(password);
-			userVo.setGender(gender);
-			
-			new UserDao().insert(userVo);
+
+		String actionName = request.getParameter("a");
+		Action action = null;
+
+		if ("joinform".equals(actionName)) {
+			action = new JoinFormAction();
+		} else if ("join".equals(actionName)) {
+			action = new JoinAction();
+
+		} else if ("joinsuccess".equals(actionName)) {
+			action = new JoinSuccessAction();
 		}
 		
+		if(action==null) {
+			//뭔갈 누른게 아니고, url을 사용자가 치고 들어온 경우.
+			response.sendRedirect(request.getContextPath());
+			//return 까먹지 말기.
+			return;
+		}
+		action.execute(request, response);
 	}
 
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		doGet(request, response);
 	}
 
