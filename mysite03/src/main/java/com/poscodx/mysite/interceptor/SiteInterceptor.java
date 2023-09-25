1ea60618b870a01e1f12acb464b27ec2a248eea2
[talkingOrange] <sonrisa-bonita@naver.com>
2023-09-25 09:52:28 +0900

    [ feat ] Interceptor - Site title issue

---
+++ mysite03/src/main/java/com/poscodx/mysite/interceptor/SiteInterceptor.java
@@ -0,0 +1,24 @@
+package com.poscodx.mysite.interceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import com.poscodx.mysite.service.SiteService;
+
+public class SiteInterceptor implements HandlerInterceptor {
+
+	@Autowired
+	private SiteService siteService;
+	
+	
+	@Override
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+			throws Exception {
+		System.out.println("siteInter");
+		return true;
+	}
+
+}
