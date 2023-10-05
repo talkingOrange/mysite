6f547c45c26f5b7d6e6b236d472800ca4d0d53e9
[talkingOrange] <sonrisa-bonita@naver.com>
2023-10-05 14:33:06 +0900

    [ feat ] init settings mysite04

---
+++ mysite04/src/main/java/com/poscodx/mysite/controller/AdminController.java
@@ -0,0 +1,72 @@
+package com.poscodx.mysite.controller;
+
+import javax.servlet.ServletContext;
+import javax.servlet.http.HttpServletRequest;
+
+import org.apache.catalina.core.ApplicationContext;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.poscodx.mysite.security.Auth;
+import com.poscodx.mysite.service.FileUploadService;
+import com.poscodx.mysite.service.SiteService;
+import com.poscodx.mysite.vo.SiteVo;
+
+@Auth(Role = "ADMIN")
+@Controller
+@RequestMapping("/admin")
+public class AdminController {
+
+	@Autowired
+	private ServletContext servletContext;
+
+	@Autowired
+	private SiteService siteService;
+
+	@Autowired
+	private FileUploadService fileUploadService;
+
+	@RequestMapping("")
+	public String main(Model model) {
+		SiteVo vo = siteService.getSite();
+		model.addAttribute("siteVo", vo);
+		System.out.println(vo);
+		return "admin/main";
+	}
+
+	@RequestMapping("/main/update")
+	public String update(SiteVo vo, MultipartFile file) {
+		String profile = fileUploadService.restore(file);
+		if (profile != null) {
+			vo.setProfile(profile);
+		}
+
+		siteService.updateSite(vo);
+		
+		// vo.title를 ServletContext에 저장
+		servletContext.setAttribute("siteTitle", vo.getTitle()); // "siteTitle"로 저장
+		
+		System.out.println("admincontroller의 img file value check: "+vo.getProfile());
+		return "redirect:/admin";
+	}
+
+	@RequestMapping("/guestbook")
+	public String guestbook() {
+		return "admin/guestbook";
+	}
+
+	@RequestMapping("/board")
+	public String board() {
+		return "admin/board";
+	}
+
+	@RequestMapping("/user")
+	public String user() {
+		return "admin/user";
+	}
+}
---
+++ mysite04/src/main/java/com/poscodx/mysite/security/Auth.java
@@ -0,0 +1,17 @@
+package com.poscodx.mysite.security;
+
+import static java.lang.annotation.ElementType.TYPE;
+import static java.lang.annotation.ElementType.METHOD;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+@Retention(RUNTIME)
+@Target({TYPE, METHOD})
+public @interface Auth {
+	public String Role() default "USER";
+	//public String value() default "";
+	public boolean test() default false;
+	
+}
